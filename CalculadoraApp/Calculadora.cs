fa1e57b7d18a3d8e61d2b7a71fe2f4a4bf2c9037
Weslley Andrade <weslleyp.andrade@gmail.com>
2024-04-27 13:13:15 +0100

    Criação projeto calculadora usando TDD

---
+++ CalculadoraApp/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraApp
+{
+    public class Calculadora
+    {
+        private List<string> listaHistorico;
+
+        public Calculadora()
+        {
+                listaHistorico = new List<string>();
+        }
+
+        public int Somar(int val1, int val2)
+        {
+            int resultado = val1 + val2;
+
+            listaHistorico.Insert(0, $"Resultado: {val1} + {val2} = {resultado}");
+
+            return resultado;
+        }
+
+        public int Subtratir(int val1, int val2)
+        {
+            int resultado = val1 - val2;
+            listaHistorico.Insert(0, $"Resultado: {val1} - {val2} = {resultado}");
+            return resultado;
+        }
+
+        public int Multiplicar(int val1, int val2)
+        {
+            int resultado = val1 * val2;
+            listaHistorico.Insert(0, $"Resultado: {val1} * {val2} = {resultado}");
+            return resultado;
+        }
+
+        public int Dividir(int val1, int val2)
+        {
+            int resultado = val1 / val2;
+            listaHistorico.Insert(0, $"Resultado: {val1} / {val2} = {resultado}");
+            return resultado;
+        }
+
+        public List<string> Historico()
+        { 
+            listaHistorico.RemoveRange(3, listaHistorico.Count-3);
+            return listaHistorico; 
+        }
+    }
+}
---
+++ CalculadoraAppTests/CalculadoraTests.cs
@@ -0,0 +1,115 @@
+﻿using CalculadoraApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraAppTests
+{
+    public class CalculadoraTests
+    {
+        private Calculadora calculadora;
+        public Calculadora InstanciaCalculadora(Calculadora calculadora)
+        {
+            if (calculadora == null)
+            {
+                return new Calculadora();
+            }
+
+            return calculadora;
+        }
+
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(5,5,10)]
+        public void TesteSomar(int val1, int val2, int result)
+        {
+            //Arrange
+            calculadora = InstanciaCalculadora(calculadora);
+
+            //Act
+
+            int resultadoCalc = calculadora.Somar(val1, val2);
+
+            //Assert
+
+            Assert.Equal(result, resultadoCalc);
+        }
+
+        [Theory]
+        [InlineData(1, 2, -1)]
+        [InlineData(5, 5, 0)]
+        public void TesteSubtratir(int val1, int val2, int result)
+        {
+            //Arrange
+            calculadora = InstanciaCalculadora(calculadora);
+            //Act
+
+            int resultadoCalc = calculadora.Subtratir(val1, val2);
+
+            //Assert
+
+            Assert.Equal(result, resultadoCalc);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(5, 5, 25)]
+        public void TesteMultiplicar(int val1, int val2, int result)
+        {
+            //Arrange
+            calculadora = InstanciaCalculadora(calculadora);
+
+            //Act
+
+            int resultadoCalc = calculadora.Multiplicar(val1, val2);
+
+            //Assert
+
+            Assert.Equal(result, resultadoCalc);
+        }
+
+        [Theory]
+        [InlineData(4, 2, 2)]
+        [InlineData(5, 5, 1)]
+        public void TesteDividir(int val1, int val2, int result)
+        {
+            //Arrange
+            calculadora = InstanciaCalculadora(calculadora);
+
+            //Act
+
+            int resultadoCalc = calculadora.Dividir(val1, val2);
+
+            //Assert
+
+            Assert.Equal(result, resultadoCalc);
+        }
+
+        [Fact]
+        public void TesteDividirPorZero()
+        {
+            calculadora = InstanciaCalculadora(calculadora);
+
+            Assert.Throws<DivideByZeroException>(() => calculadora.Dividir(3,0));
+        }
+        
+        [Fact]
+        public void TesteHistorico()
+        {
+            calculadora = InstanciaCalculadora(calculadora);
+
+            calculadora.Somar(1, 3);
+            calculadora.Somar(2, 2);
+            calculadora.Somar(3, 1);
+            calculadora.Somar(4, 8);
+            
+            var lista = calculadora.Historico();
+
+            Assert.NotEmpty(lista);
+            Assert.Equal(3, lista.Count);
+        }
+
+    }
+}
